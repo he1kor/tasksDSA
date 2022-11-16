34d55a17c9d306873aefaf1aaa1dceb089ed9c78
Helkor <helkor.rightway@gmail.com>
2022-11-16 04:12:52 +0300

    A bit code improved

--- main.cpp
+++ main.cpp
@@ -6,15 +6,17 @@ int main()
 {
     TaskSelector taskSelector = TaskSelector();
     const std::vector<std::string> tasks = taskSelector.getTasks();
-    std::string answer;
-    while (true) {
+
+    while (true)
+    {
         std::cout << "List of tasks: ";
         Printer::printVector(tasks);
         std::cout << "Choose a task:";
-        answer = Input::loopQuestion<TaskSelector>(&TaskSelector::hasTask,taskSelector,"0");
+        std::string answer = Input::loopQuestion<TaskSelector>(&TaskSelector::hasTask,taskSelector,"0");
         if (answer == "__EXIT__")
             break;
         taskSelector.launch(answer);
     }
+
     return 0;
 }
